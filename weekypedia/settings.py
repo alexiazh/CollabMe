ae0849cb7a3a2439c53ae7609673962b04c008da
alexiazh <alexiazhou726@ucla.edu>
2022-04-02 07:32:35 -0700

    Delete staticfiles

--- weekypedia/settings.py
+++ weekypedia/settings.py
@@ -134,8 +134,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
